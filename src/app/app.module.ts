53670cd28a6ce49448a3001336fb5fc84bb9780f
mouhamedNoire <mouhamedabdallah.bathilly@uvs.edu.sn>
2023-04-30 21:24:05 +0200

    adding services and HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductListComponent } from './products/product-list/product-list.compo
 import {FormsModule} from "@angular/forms";
 import {convertToSpacesPipe} from "./shared/convert-to-spaces-pipe";
 import {StarComponent} from "./shared/star.component";
+import{HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {StarComponent} from "./shared/star.component";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
